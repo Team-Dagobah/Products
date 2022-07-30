8179a4682a3a40e7f85be13443e3cc0da4a56755
Fahad Syed <syed.216@gmail.com>
2022-07-30 11:54:17 -0500

    Added bluebird and promisfied queries

--- db.js
+++ db.js
@@ -1,11 +1,18 @@
 const {Client} = require('pg');
+const Promise = require("bluebird");
 
-const db = new Client({
+const connection = new Client({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.PORT
 })
+
+const db = Promise.promisifyAll(connection, { multiArgs: true });
+
+db.connectAsync()
+  .then(() => console.log(`Connected to PostGres at port: ${process.env.PORT}`))
+  .catch((err) => console.log(`DB Connection ERROR`, err))
 
 module.exports = db
--- server.js
+++ server.js
@@ -1,31 +1,54 @@
 require("dotenv").config();
 const express = require("express");
 
-const db = require('./db.js')
+const db = require('./db')
 
 const app = express();
 
 app.use(express.json())
 
-db.connect();
 
 app.listen(3000, ()=> console.log("Listening at port 3000"))
 
+// app.get('/products', (req, res) => {
+//   let page = req.query.page || 1
+//   let count = req.query.count || 5
+//   if(page === 1){
+//     db.query(`Select * from products order by id asc limit ${count}`, (err, result) =>{
+//       if(!err) {
+//         res.status(200).send(result.rows)
+//       } else{
+//         console.log(err)
+//       }
+//     });
+//   } else {
+//     db.query(`Select * from products where id > ${(page - 1)*count} order by id asc limit ${count}`, (err, result) =>{
+//       if(!err) {
+//         res.status(200).send(result.rows)
+//       }else{
+//         console.log(err)
+//       }
+//     });
+//   }
+// })
 app.get('/products', (req, res) => {
   let page = req.query.page || 1
   let count = req.query.count || 5
   if(page === 1){
-    db.query(`Select * from products order by id asc limit ${count}`, (err, result) =>{
-      if(!err) {
-        res.status(200).send(result.rows)
-      }
-    });
+    return db.queryAsync(`EXPLAIN ANALYZE Select * from products order by id asc limit ${count}`)
+    .then(result => result[0])
+    .then((result) => {
+      res.status(200).send(result.rows)
+    })
+    .catch((err) => console.log(err))
   } else {
-    db.query(`Select * from products where id > ${(page - 1)*count} order by id asc limit ${count}`, (err, result) =>{
-      if(!err) {
-        res.status(200).send(result.rows)
-      }
-    });
+    return db.queryAsync(`EXPLAIN ANALYZE Select * from products where id > ${(page - 1)*count} order by id asc limit ${count}`)
+    .then(result => result[0])
+    .then((result) => {
+      res.status(200).send(result.rows)
+    })
+    .catch((err) => console.log(err))
   }
-  // db.end;
 })
+
+//add EXPLAIN ANALYZE in front of query to see
