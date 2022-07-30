763d155c4b5ff27c580e76ae938c0cbbd6a02f84
Fahad Syed <syed.216@gmail.com>
2022-07-30 14:27:23 -0500

    Added relatedproductsroute

--- db.js
+++ db.js
@@ -15,4 +15,23 @@ db.connectAsync()
   .then(() => console.log(`Connected to PostGres at port: ${process.env.PORT}`))
   .catch((err) => console.log(`DB Connection ERROR`, err))
 
-module.exports = db
+let getRelatedProducts = (current_product_id) => {
+  return db.queryAsync(`SELECT related_product_id FROM related
+    where current_product_id = ${current_product_id}`)
+    .then(results => {return results[0]})
+    .catch(err => console.log(err))
+}
+
+let getProductsListDefaultPage = (count) => {
+  return db.queryAsync(`Select * from products order by id asc limit ${count}`)
+  .then(result => result[0])
+  .catch(err => console.log(err))
+}
+
+let getProductsListCustomPage = (count, page) => {
+  return db.queryAsync(`Select * from products where id > ${(page - 1)*count} order by id asc limit ${count}`)
+  .then(result => result[0])
+  .catch(err => console.log(err))
+}
+
+module.exports = {db, getRelatedProducts, getProductsListCustomPage, getProductsListDefaultPage};
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 
-const db = require('./db')
+const db = require('./db.js')
 
 const app = express();
 
 app.use(express.json())
-
 
 app.listen(3000, ()=> console.log("Listening at port 3000"))
 
@@ -35,20 +34,27 @@ app.get('/products', (req, res) => {
   let page = req.query.page || 1
   let count = req.query.count || 5
   if(page === 1){
-    return db.queryAsync(`EXPLAIN ANALYZE Select * from products order by id asc limit ${count}`)
-    .then(result => result[0])
+    db.getProductsListDefaultPage(count)
     .then((result) => {
       res.status(200).send(result.rows)
     })
     .catch((err) => console.log(err))
   } else {
-    return db.queryAsync(`EXPLAIN ANALYZE Select * from products where id > ${(page - 1)*count} order by id asc limit ${count}`)
-    .then(result => result[0])
+    db.getProductsListCustomPage(count, page)
     .then((result) => {
       res.status(200).send(result.rows)
     })
     .catch((err) => console.log(err))
   }
+})
+
+
+app.get(`/products/:product_id`, (req, res) =>{
+  db.getRelatedProducts(req.params.product_id)
+  .then((result) => {
+    res.status(200).send(result.rows)
+  })
+  .catch(err => console.log(err))
 })
 
 //add EXPLAIN ANALYZE in front of query to see
